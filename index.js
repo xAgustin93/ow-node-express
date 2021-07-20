399bde0e2ecb0a5d38d748c5b909b3ac99e0bd9a
Agustin Navarro Galdon <xAgustin93@gmail.com>
2021-07-20 19:42:46 +0200

    Deploy

---
+++ index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const port = 3000;
+const urlMongoDb =
+  "mongodb+srv://admin:admin123456@taskdb.p0hyu.mongodb.net/mydb";
+
+mongoose.connect(
+  urlMongoDb,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err, res) => {
+    try {
+      if (err) {
+        throw err;
+      } else {
+        console.log("La conexion a la base de datos es correcta");
+
+        app.listen(port, () => {
+          console.log(
+            "Servidor del API REST esta funcionando en http://localhost:3000"
+          );
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
